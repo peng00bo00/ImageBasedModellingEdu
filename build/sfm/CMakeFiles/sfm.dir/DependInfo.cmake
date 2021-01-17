abddfae688dd14e283afa7c034e810f3095f5228
peng00bo00 <peng00bo00@git.com>
2021-01-17 10:13:24 +0400

    add files

---
+++ build/sfm/CMakeFiles/sfm.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pengbo/ImageBasedModellingEdu/sfm/ba_linear_solver.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/ba_linear_solver.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/bundle_adjustment.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/bundle_adjustment.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/bundler_common.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/bundler_common.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/bundler_features.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/bundler_features.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/bundler_incremental.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/bundler_incremental.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/bundler_init_pair.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/bundler_init_pair.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/bundler_intrinsics.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/bundler_intrinsics.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/bundler_matching.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/bundler_matching.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/bundler_tracks.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/bundler_tracks.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/camera_database.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/camera_database.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/extract_focal_length.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/extract_focal_length.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/feature_set.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/feature_set.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/fundamental.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/fundamental.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/homography.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/homography.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/pose_p3p.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/pose_p3p.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/ransac.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/ransac.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/ransac_fundamental.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/ransac_fundamental.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/ransac_homography.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/ransac_homography.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/ransac_pose_p3p.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/ransac_pose_p3p.cc.o"
+  "/home/pengbo/ImageBasedModellingEdu/sfm/triangulate.cc" "/home/pengbo/ImageBasedModellingEdu/build/sfm/CMakeFiles/sfm.dir/triangulate.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "../sfm/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
